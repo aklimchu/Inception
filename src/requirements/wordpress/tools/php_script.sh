5e785892c50bb1368349ad1cf3074d5bb01a06d7
aklimchu <aklimchu@aklimchu.42.fr>
2025-04-01 16:38:25 +0300

    mounted volumes to Home

--- src/requirements/wordpress/tools/php_script.sh
+++ src/requirements/wordpress/tools/php_script.sh
@@ -27,7 +27,7 @@ mysql -h mariadb -u root -ppassword1234 -e "SHOW DATABASES;" || { echo "MySQL co
 if [ ! -f "wp-config.php" ]; then
   php82 -d memory_limit=256M /usr/local/bin/wp core download --allow-root || { echo "Failed to download WordPress"; exit 1; }
   php82 -d memory_limit=256M /usr/local/bin/wp config create --dbname=wordpress --dbuser=root --dbpass=password1234 --dbhost=mariadb --allow-root || { echo "Failed to create wp-config.php"; exit 1; }
-  php82 -d memory_limit=256M /usr/local/bin/wp core install --url=localhost --title="Inception" \
+  php82 -d memory_limit=256M /usr/local/bin/wp core install --url=https://aklimchu.42.fr --title="Inception" \
       --admin_user=supervisor --admin_password=superpass --admin_email=supervisor@example.com --allow-root || { echo "Failed to install WordPress"; exit 1; }
 
   # Add a second user (wp_user) only if it doesn't exist
